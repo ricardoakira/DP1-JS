6976e43a95eeeaa0273741ea042043d3a03a3d68
ricardoakira <rm551447@fiap.com.br>
2024-06-11 10:46:27 -0300

    ex3

--- script.js
+++ script.js
@@ -25,3 +25,15 @@
 //     console.log("Reprovado")
 // }
 
+// 3-
+// let altura = 1.80
+// let peso = 80
+// let imc = peso/altura**2
+// console.log("IMC: "+imc.toFixed(2))
+// if(imc<18.5){
+//     console.log("Abaixo do peso")
+// }else if(imc>=18.5 && imc<=24.9){
+//     console.log("Peso ideal")
+// }else{
+//     console.log("Acima do peso")
+// }
