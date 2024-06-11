d609abbaa331caceb52991c4046aec33e24afed4
ricardoakira <rm551447@fiap.com.br>
2024-06-11 10:46:09 -0300

    ex2

--- script.js
+++ script.js
@@ -6,3 +6,22 @@
 // console.log(valorVazio)
 // console.log(valor)
 
+// 2 -
+// let idade = 17
+// if(idade!=18){
+//     console.log("Não tem 18 anos")
+// }
+
+// let nomeFaculdade = "FIAP"
+// let faculdade = "fiap"
+// if(faculdade.toUpperCase()==nomeFaculdade){
+//     console.log("Estuda na FIAP")
+// }
+
+// let nota = 7
+// if(nota>=6){
+//     console.log("Aprovado")
+// }else{
+//     console.log("Reprovado")
+// }
+
